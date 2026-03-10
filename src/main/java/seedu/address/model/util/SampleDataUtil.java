e23afc30b01ed8ddd23618b0dc448b826aa8eae5
davinkhor <73937826+deltaMinor@users.noreply.github.com>
2026-03-10 21:12:24 +0800

    style: Amend import checkstyle

--- src/main/java/seedu/address/model/util/SampleDataUtil.java
+++ src/main/java/seedu/address/model/util/SampleDataUtil.java
@@ -10,8 +10,8 @@ import seedu.address.model.contact.Address;
 import seedu.address.model.contact.Contact;
 import seedu.address.model.contact.Email;
 import seedu.address.model.contact.Name;
-import seedu.address.model.contact.Phone;
 import seedu.address.model.contact.Notes;
+import seedu.address.model.contact.Phone;
 import seedu.address.model.tag.Tag;
 
 /**
