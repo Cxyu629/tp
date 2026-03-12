dd564f9e62107b9ec68890128b21d679b8ceea37
Justin <usingenglish.2014@gmail.com>
2026-03-12 20:10:56 +0800

    test: Add coverage for successful note parse

--- src/test/java/seedu/address/logic/parser/NoteCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/NoteCommandParserTest.java
@@ -4,17 +4,55 @@ import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.commands.NoteCommand.PREFIX_CLEAR;
 import static seedu.address.logic.commands.NoteCommand.PREFIX_REMOVE;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
+import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_CONTACT;
 
 import org.junit.jupiter.api.Test;
 
+import seedu.address.logic.commands.NoteAddCommand;
+import seedu.address.logic.commands.NoteClearCommand;
 import seedu.address.logic.commands.NoteCommand;
+import seedu.address.logic.commands.NoteRemoveCommand;
+import seedu.address.model.contact.Note;
 
 public class NoteCommandParserTest {
     private static final String NOTES_STRING = "To follow up on Wednesday";
     private static final String MESSAGE_INVALID_FORMAT =
             String.format(MESSAGE_INVALID_COMMAND_FORMAT, NoteCommand.MESSAGE_USAGE);
+    private static final String NUM_LINES_STRING = "1";
+    private static final int NUM_LINES = 1;
 
     private NoteCommandParser parser = new NoteCommandParser();
+
+    @Test
+    public void parse_noteAddCommand_success() {
+        NoteAddCommand expectedNoteAddCommand =
+            new NoteAddCommand(INDEX_FIRST_CONTACT, new Note(NOTES_STRING));
+        assertParseSuccess(
+            parser,
+            "1 " + NOTES_STRING,
+            expectedNoteAddCommand);
+    }
+
+    @Test
+    public void parse_noteRemoveCommand_success() {
+        NoteRemoveCommand expectedNoteRemoveCommand =
+            new NoteRemoveCommand(INDEX_FIRST_CONTACT, NUM_LINES);
+        assertParseSuccess(
+            parser,
+            "1 " + PREFIX_REMOVE + NUM_LINES_STRING,
+            expectedNoteRemoveCommand);
+    }
+
+    @Test
+    public void parse_noteClearCommand_success() {
+        NoteClearCommand expectedNoteClearCommand =
+            new NoteClearCommand(INDEX_FIRST_CONTACT);
+        assertParseSuccess(
+            parser,
+            "1 " + PREFIX_CLEAR + NOTES_STRING,
+            expectedNoteClearCommand);
+    }
 
     @Test
     public void parse_missingParts_failure() {
